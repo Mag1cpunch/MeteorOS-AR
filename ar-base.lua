fd6b7bcdd46807244a37d1097e0c2128bb35f949
Meteor7283 <99803083+Mag1cpunch@users.noreply.github.com>
2023-11-19 19:20:07 +0200

    Update ar-base.lua

--- ar-base.lua
+++ ar-base.lua
@@ -4,18 +4,21 @@ local plrdtc = peripheral.find("playerDetector")
 local function update()
     arctrl.clear()
     arctrl.drawString("MeteorOS - Version 1.0", 0, 0, 255)
+
     if plrdtc then
         local plrs = plrdtc.getOnlinePlayers()
         local postable = {}
+
         if next(plrs) == nil then
             arctrl.drawString("No players in range", 0, -1, 255)
         else
-            for i in ipairs(plrs) do
-                local plrpos = plrs.getPosition(i)
-                table.insert(postable, i..": X:"..plrpos.x..", Y:"..plrpos.y)
+            for _, playerName in ipairs(plrs) do
+                local plrpos = plrdtc.getPlayerPos(playerName)
+                table.insert(postable, playerName .. ": X:" .. plrpos.x .. ", Y:" .. plrpos.y)
                 os.sleep(0.1)
             end
         end
+
         arctrl.drawString(postable, 0, 1, 255)
     end
 end
