5191c7e3b234f8cd5041942923c3fde9f8baabd8
Meteor7283 <99803083+Mag1cpunch@users.noreply.github.com>
2023-11-19 19:54:49 +0200

    Update ar-base.lua

--- ar-base.lua
+++ ar-base.lua
@@ -4,22 +4,22 @@ local plrdtc = peripheral.find("playerDetector")
 --Update the glasses display every 2 seconds
 local function update()
     arctrl.clear()
-    arctrl.drawString("MeteorOS - Version 1.0", 0, 0, 255)
+    arctrl.drawString("MeteorOS - Version 1.0", 50, 0, 255)
 
     if plrdtc then
         local plrs = plrdtc.getOnlinePlayers()
         local postable = {}
 
         if not next(plrs) then
-            arctrl.drawString("No players found", 0, -1, 255)
+            arctrl.drawString("No players found", 40, -1, 255)
         else
             for _, playerName in ipairs(plrs) do
                 local plrpos = plrdtc.getPlayerPos(playerName)
-                table.insert(postable, playerName .. ": X:" .. plrpos.x .. ", Y:" .. plrpos.y..", Z:".. plrpos.z)
+                table.insert(postable,playerName .. ": X:" .. plrpos.x .. ", Y:" .. plrpos.y..", Z:".. plrpos.z)
                 os.sleep(0.1)
             end
             local joinedString = table.concat(postable, "\n")
-            arctrl.drawString(joinedString, 0, 15, 255)
+            arctrl.drawString("Server Players: "..joinedString, 40, 15, 255)
         end
     end
 end
