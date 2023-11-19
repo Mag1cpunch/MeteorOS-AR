e6e72f545f2cb4995d3204bd193077f5e5c7defb
Meteor7283 <99803083+Mag1cpunch@users.noreply.github.com>
2023-11-19 18:59:22 +0200

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,9 +1,18 @@
+if fs.exists("/ar-base.lua") then
+    term.write("Deleted old ar-base.lua...")
+    fs.delete("/ar-base.lua")
+end
+if fs.exists("/startup.lua") then
+    term.write("Deleted old startup.lua...")
+    fs.delete("/startup.lua")
+end
 term.write("Installing ar-base.lua...")
 shell.run("wget https://raw.githubusercontent.com/Mag1cpunch/MeteorOS-AR/main/ar-base.lua")
-term.write("Installed")
-term.write("Installing startup.lua...")
+term.write("\nInstalled")
+os.sleep(1)
+term.write("\nInstalling startup.lua...")
 shell.run("wget https://raw.githubusercontent.com/Mag1cpunch/MeteorOS-AR/main/startup.lua")
-term.write("Installed")
-term.write("Rebooting...")
+term.write("\nInstalled")
+term.write("\nRebooting...")
 os.sleep(3)
 os.reboot()
